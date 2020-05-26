4c72e79f29d86db1846899dc1bacf8c8ce7d47b2
Pieter Smets <mail@pietersmets.be>
2020-05-26 09:55:37 +0200

    queue crontab fix

--- pyvdms/jobber/queue.py
+++ pyvdms/jobber/queue.py
@@ -101,7 +101,7 @@ class Queue(object):
     def crontab(self, crontab: str = None):
         """
         """
-        if not isinstance(crontab, str) or contab is not None:
+        if not (isinstance(crontab, str) or contab is None):
             raise TypeError('crontab should be of type `str`.')
         self._cron = crontab
 
